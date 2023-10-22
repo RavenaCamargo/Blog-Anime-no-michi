7b6b39a7210ee66f0c2170f6fa823bdf0603182d
Ravena <99302081+RavenaCamargo@users.noreply.github.com>
2023-10-21 21:27:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Seu código JavaScript aqui
-});
-
 // Função para abrir uma guia
 function openTab(event, tabName) {
     const tabcontent = document.getElementsByClassName("tabcontent");
     for (let i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
-    }
-
-    // Exibir os títulos das subguias se a guia "Recomendações" for selecionada
-    if (tabName === "tab2") {
-        const subtabtitles = document.getElementsByClassName("subtab");
-        for (let i = 0; i < subtabtitles.length; i++) {
-            subtabtitles[i].style.display = "block";
-        }
     }
 
     document.getElementById(tabName).style.display = "block";
@@ -35,27 +23,13 @@ function openSubTab(subTabName) {
     }
 }
 
+//Função para abrir a página direto na Home
 function openSubTabFromHome(element) {
-    console.log("openSubTabFromHome called"); // Adicione este log
+    console.log("openSubTabFromHome called"); 
     const subTabName = element.getAttribute("data-subtab");
     const subtabcontent = document.getElementsByClassName("subtabcontent");
     for (let i = 0; i < subtabcontent.length; i++) {
         subtabcontent[i].style.display = "none";
     }
     document.getElementById(subTabName).style.display = "block";
-
-    // Ocultar os títulos das subguias após abrir a subguia
-    const subtabtitles = document.getElementsByClassName("subtab");
-    for (let j = 0; j < subtabtitles.length; j++) {
-        subtabtitles[j].style.display = "none";
-    }
 }
-
-// Adicione um evento de clique a todos os elementos da guia Home
-const tab1GridItems = document.querySelectorAll("#tab1 .grid-item");
-
-tab1GridItems.forEach(function (element) {
-    element.addEventListener("click", function () {
-        openSubTabFromHome(element);
-    });
-});
